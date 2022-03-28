f29863dd0971f6ac58eb028ebc1ff4eef2f69922
Raniket Ram <raniketram@197NODMB27479.local>
2022-03-28 21:51:47 +0530

    deploy script

--- .github/scripts/deploy.sh
+++ .github/scripts/deploy.sh
@@ -5,6 +5,6 @@ ssh -V
 # don't want to be prompted by ssh for first time remote hosts.
 # alias ssh='ssh -o StrictHostKeyChecking=no'
 
-# echo "SSH key: $EC2_SSH_KEY"
+echo "SSH key: $EC2_SSH_KEY"
 
 echo "DONE....."
